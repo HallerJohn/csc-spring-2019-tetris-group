311adcbb27071dd76b71288197c4fdc8540ad8e9
John Haller <4jfhaller@gmail.com>
2019-03-12 20:08:01 -0700

    Signed-off-by: John Haller <4jfhaller@gmail.com>

---
+++ Master/Tetris/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <?php   include 'Field.php';   ?>
+        <style>
+            body {
+              background-color: white;
+              text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <p style="display: inline-table;
+           border:5px black;
+           line-height: 0;
+           border-style: solid;
+           border-color: black;
+           box-sizing: content-box;">
+            <?php
+        
+            $object = new Field();
+            ?>
+        </p>
+    </body>
+</html>
