0b6d7b630d7cb5deba2cb234d2cc02ed04103efd
John Haller <4jfhaller@gmail.com>
2019-03-20 15:12:19 -0700

    Signed-off-by: John Haller <4jfhaller@gmail.com>

--- Master/Tetris/public_html/game.js
+++ Master/Tetris/public_html/game.js
@@ -15,7 +15,7 @@ context.scale(20,20);
 var field = [
     [0,0,0],
     [1,1,1],
-    [0,1,0],
+    [0,1,0]
 ];
 
 function draw(){
@@ -24,31 +24,61 @@ function draw(){
     writeField(playerData.field,playerData.position);
 }
 
-function updateField(){
-    draw();
-    requestAnimationFrame(updateField);
-//    setInterval(autoDrop(),1000000000);
-}
+var fps=1;//drops per second
+var now;
+var then=Date.now();
+var interval=1000/fps;
+var delta;
 
-function autoDrop(){
-    playerData.position.y+=1;
+function updateField(){
+    requestAnimationFrame(updateField);
+    now=Date.now();
+    delta=now-then;
+    if(delta>interval){
+        then=now-(delta%interval);
+        move('y',1);
+    }
+    draw();    
 }
 
 //writing the block to the canvas
 function writeField(field,adjust){
     field.forEach((row,y)=>{
         row.forEach((value,x)=>{
-            if(value!=0){
+            if(value!==0){
                 context.fillStyle='red';
                 context.fillRect(x+adjust.x,y+adjust.y,1,1);
             }
-        })
-    })
+        });
+    });
 }
 
 var playerData={
     position: {x:5,y:0},
-    field: field,
+    field: field
+};
+function move(axis,dir){
+    if(axis==='x')playerData.position.x+=dir;
+    else if(axis==='y')playerData.position.y+=dir;
+    console.log("X val="+playerData.position.x);
 }
 
 updateField();
+
+document.onkeydown=function(event){
+    switch(event.keyCode){
+        case 37:{
+                move('x',-1);
+                break;
+        }case 39:{
+                move('x',1);
+                break;
+        }case 40:{
+                move('y',1);
+                break;
+        }case 38:{
+                move('y',-1);//TESTING ONLY REMOVE BEFORE FINAL GAME!!!!
+        }
+    }
+};
+    
